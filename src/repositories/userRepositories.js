35d7c88c5d70a493c2860a8b8d6c96b3417efe18
codethi <docencia.thiago@gmail.com>
2023-03-28 21:06:33 -0300

    feat: user repository function create

--- src/repositories/userRepositories.js
+++ src/repositories/userRepositories.js
@@ -9,7 +9,15 @@ async function findByEmail(email) {
   );
 }
 
-async function create({name, email, password}) {}
+async function create({ name, email, password }) {
+  await connectionDb.query(
+    `
+        INSERT INTO users (name, email, password)
+        VALUES ($1, $2, $3)
+    `,
+    [name, email, password]
+  );
+}
 
 export default {
   findByEmail,
