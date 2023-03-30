ea501569899b353bc20e0e43a8bd0b57449f8690
codethi <docencia.thiago@gmail.com>
2023-03-30 20:01:44 -0300

    refact: name error in schema validation

--- src/middlewares/schemaValidationMiddleware.js
+++ src/middlewares/schemaValidationMiddleware.js
@@ -1,11 +1,11 @@
-import errors from "../errors/index.js";
+import err from "../errors/index.js";
 
 export function validateSchema(schema) {
   return (req, res, next) => {
     const { error } = schema.validate(req.body, { abortEarly: false });
     if (error) {
       const errors = error.details.map((detail) => detail.message);
-      throw errors.conflictError(errors);
+      throw err.conflictError(errors);
     }
 
     next();
